5a1f16b0267a14a66178b179aa1cc09b46eb6918
mr.hunt <sahuntjr@gmail.com>
2015-12-06 20:20:23 -0800

    Add gems and modified files to Heroku

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ActiveRecord::Base
-    has_attached_file :image, styles: { medium: "100x>", thumb: "100x100>" }, default_url: "default.jpeg"
+    has_attached_file :image, styles: { medium: "200x>", thumb: "200x200>" }, default_url: "default.jpeg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   end
